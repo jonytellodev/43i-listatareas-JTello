912dc949550e4d36ab7bc11f58ae9a6e19c29d89
jonytellodev <jonathantello.dev@gmail.com>
2023-07-24 20:31:00 -0300

    se agrega ejemplos de useEffect

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,7 +1,14 @@
 import { ListGroup, Button } from "react-bootstrap";
 
-const ItemTarea = ({textoTarea, borrarTarea}) => {
-  return <ListGroup.Item className="d-flex justify-content-between mt-3 bg-">{textoTarea} <Button variant="danger" onClick={()=>borrarTarea(textoTarea)}>Borrar</Button></ListGroup.Item>;
+const ItemTarea = ({ textoTarea, borrarTarea }) => {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between mt-3 bg-">
+      {textoTarea}{" "}
+      <Button variant="danger" onClick={() => borrarTarea(textoTarea)}>
+        Borrar
+      </Button>
+    </ListGroup.Item>
+  );
 };
 
 export default ItemTarea;
